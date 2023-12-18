6120925b36c7f35190881f31355b67257304bca2
Hyunjin0320 <85651905+Hyunjin0320@users.noreply.github.com>
2023-12-18 14:12:25 +1300

    vickypush

--- frontend/src/components/pages/Contact.jsx
+++ frontend/src/components/pages/Contact.jsx
@@ -1,46 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../common/Layout";
 import "./Contact.css";
 import { SendOutlined } from "@ant-design/icons";
 
 const Contact = () => {
+  // Track the active button
+  const [activeSubject, setActiveSubject] = useState("General Inquiry");
+
+  const handleSubjectClick = (subject) => {
+    setActiveSubject(subject);
+  };
   return (
     <Layout>
       <div className="contact-container">
         <h1>Contact Us</h1>
-        <p>Any question or remarks? Just write us a message!</p>
+        <p className="custom-message">
+          Any question or remarks? Just write us a message!
+        </p>
         <div className="contact-form">
+          {/* Input group for name fields */}
           <div className="input-group">
             <input type="text" placeholder="First Name" />
             <input type="text" placeholder="Last Name" />
           </div>
+          {/* Input group for email and phone number */}
           <div className="input-group">
             <input type="email" placeholder="Email" />
             <input type="tel" placeholder="Phone Number" />
           </div>
+          {/* Subject selection group */}
           <div className="subject-group">
             <label>Select Subject?</label>
             <div className="subject-options">
-              {/* Message */}
-              <button className="subject-button">General Inquiry</button>
-              <button className="subject-button">Technique Issue</button>
+              <button
+                className={`subject-button ${
+                  activeSubject === "General Inquiry" ? "active" : ""
+                }`}
+                onClick={() => handleSubjectClick("General Inquiry")}
+              >
+                General Inquiry
+              </button>
+              <button
+                className={`subject-button ${
+                  activeSubject === "Technique Issue" ? "active" : ""
+                }`}
+                onClick={() => handleSubjectClick("Technique Issue")}
+              >
+                Technique Issue
+              </button>
             </div>
           </div>
-          <textarea placeholder="Write your message..."></textarea>
-          {/* Send Message */}
+          {/* Message input group */}
+          <div className="message-group">
+            <textarea placeholder="Write your message..."></textarea>
+          </div>
+          {/* Send message button */}
           <button className="send-message">
             <SendOutlined /> Send Message
           </button>
         </div>
+        {/* Contact information section */}
         <div className="contact-info">
-          {/* Informaation */}
           <p>
-            <strong>Reach us</strong>: +1012 3456 789
+            <strong>Reach us:</strong> +1012 3456 789
           </p>
+          <p>demo@gmail.com</p> {/* Add email address */}
+          <p>Unleash Space 20 Symonds Street</p> {/* Add address */}
+          <p>Auckland CBD, Auckland 1010</p>{" "}
         </div>
       </div>
     </Layout>
   );
 };
 
+// Exporting the component
 export default Contact;
--- frontend/src/components/pages/report_page/ReportPage.jsx
+++ frontend/src/components/pages/report_page/ReportPage.jsx
@@ -1,19 +1,39 @@
+import React from "react";
 import OverallWaste from "./OverallWaste.jsx";
 import NearingExpiration from "./NearingExpiration.jsx";
 import WasteOverTime from "./WasteOverTime.jsx";
 import MostWastedItem from "./MostWastedItem.jsx";
 import Layout from "../../common/Layout.jsx";
+import "./ReportPage.css";
 
-const ReportPage = () =>{
-    return(
-      <>
-          <Layout></Layout>
-          <OverallWaste/>
-          <NearingExpiration/>
-          <WasteOverTime/>
-          <MostWastedItem/>
-      </>
-    );
-}
+const ReportPage = () => {
+  return (
+    <Layout>
+      <div className="report-container">
+        <h1 className="report-title">Report</h1>
+        <div className="top-section">
+          <div className="overall-waste-container">
+            <div className="overall-waste-title">Overview</div>
+            <div className="overall-waste-value">
+              <OverallWaste /> {/* Just for the value */}
+            </div>
+            <div className="overall-waste-label">Total Waste</div>
+          </div>
+          <div className="nearing-expiration-container">
+            <h3 className="NearingExpiration-title">Food Nearing Expiration</h3>
+            <NearingExpiration />
+          </div>
+        </div>
+        <div className="waste-over-time-container">
+          <h3 className="wasteOT-title">Waste Over Time</h3>
+          <WasteOverTime />
+        </div>
+        <div className="most-wasted-item-container">
+          <MostWastedItem />
+        </div>
+      </div>
+    </Layout>
+  );
+};
 
 export default ReportPage;
